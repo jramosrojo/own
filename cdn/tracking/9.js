e2b30a269a0a2c12e141b9fc6999992f8958c52c
Javier Ramos Rojo <46815369+jramosr@users.noreply.github.com>
2019-10-16 18:54:30 +0200

    jramosrojo.net

--- cdn/tracking/9.js
+++ cdn/tracking/9.js
@@ -3,7 +3,7 @@ var _paq = window._paq || [];
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//stats.ramosrojo.net/";
+    var u="//stats.jramosrojo.net/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '9']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
