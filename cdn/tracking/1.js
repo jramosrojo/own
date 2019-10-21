13fd203c0ec62d3fbd44f4261fcbcc43d51f75dd
Javier Ramos Rojo <46815369+jramosr@users.noreply.github.com>
2019-10-21 23:09:05 +0200

    -

--- cdn/tracking/1.js
+++ cdn/tracking/1.js
@@ -1,4 +1,6 @@
-var _paq = window._paq || [];
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
@@ -9,3 +11,5 @@ var _paq = window._paq || [];
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
+</script>
+<!-- End Matomo Code -->
